f9088274c5de38e96cce545124e867154e20d595
Oscar Kalldal <oscar@kalldal.se>
2014-05-06 21:46:55 +0200

    normalized GraphDist

--- GraphDist.m
+++ GraphDist.m
@@ -2,6 +2,6 @@ function [ d ] = GraphDist( A, B )
 %GRAPHSIM Calculates the distance between two graphs
 %   Detailed explanation goes here
   
-d = sum(sum(A) + sum(B) - 2*sum(A.*B));
+d = sum(sum(A) + sum(B) - 2*sum(A.*B))/sum(sum(A) + sum(B));
 
 end
